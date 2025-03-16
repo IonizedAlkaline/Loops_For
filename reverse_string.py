c96f53d566e6ee0e7ea6e4069b90747eff116306
IonizedAlkaline <rudranshm007@gmail.com>
2025-03-15 19:09:50 -0700

    Add files via upload

---
+++ reverse_string.py
@@ -0,0 +1,7 @@
+word = input('Enter a word: ')
+x =''
+
+for y in word:
+    
+    x= y+x
+print(x)
---
+++ whole_numbers.py
@@ -0,0 +1,5 @@
+number = int(input('Enter a whole number: '))
+sum = 0
+for x in range(1,number+1):
+    sum = sum + x 
+print("The value of the given number was", sum)
